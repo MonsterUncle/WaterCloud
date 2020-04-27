1c77b736011d6e959bbae4dd0a07837443144f51
钱玮鸿 <7353672+qian_wei_hong@users.noreply.gitee.com>
2020-04-27 14:51:46 +0800

    定时器修改

--- WaterCloud.Application/TimeService/TimedService.cs
+++ WaterCloud.Application/TimeService/TimedService.cs
@@ -30,6 +30,8 @@ namespace WaterCloud.Service.TimeService
 
         private readonly ILogger _logger;
         private readonly IHostingEnvironment _hostingEnvironment;
+        //定时器
+        private Timer _timer;
 
         //private Timer _timer;
 
@@ -39,27 +41,29 @@ namespace WaterCloud.Service.TimeService
             _hostingEnvironment=hostingEnvironment;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("servers start");
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                //执行任务
-                ServerStateEntity entity = new ServerStateEntity();
-                var computer = ComputerHelper.GetComputerInfo();
-                entity.F_ARM = computer.RAMRate;
-                entity.F_CPU = computer.CPURate;
-                entity.F_IIS = "0";
-                entity.F_WebSite = _hostingEnvironment.ContentRootPath;
-                new ServerStateService().SubmitForm(entity);
-                await Task.Delay(30000, stoppingToken); //延迟暂停30秒
-            }
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(30));//间隔30秒
             _logger.LogInformation("servers end");
+            return Task.CompletedTask;
+        }
+        private void DoWork(object state)
+        {
+            //执行任务
+            ServerStateEntity entity = new ServerStateEntity();
+            var computer = ComputerHelper.GetComputerInfo();
+            entity.F_ARM = computer.RAMRate;
+            entity.F_CPU = computer.CPURate;
+            entity.F_IIS = "0";
+            entity.F_WebSite = _hostingEnvironment.ContentRootPath;
+            new ServerStateService().SubmitForm(entity);
         }
 
         public override void Dispose()
         {
             base.Dispose();
+            _timer?.Dispose();
         }
 
     }
