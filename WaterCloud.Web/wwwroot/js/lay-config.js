ef9772e828ea14beb7b5713628b215249339557e
钱玮鸿 <7353672+qian_wei_hong@users.noreply.gitee.com>
2020-05-11 09:36:45 +0800

    添加注释

--- WaterCloud.Web/wwwroot/js/lay-config.js
+++ WaterCloud.Web/wwwroot/js/lay-config.js
@@ -29,6 +29,6 @@ layui.config({
     common: 'waterCloud/common', //  工具类
     tablePlug: 'tablePlug/tablePlug', //  tabl扩展
     dtree: 'dtree/dtree', //  树形扩展
-    xmSelect:'xm-select/xm-select'
+    xmSelect:'xm-select/xm-select' //select多选扩展
     //optimizeSelectOption: 'tablePlug/optimizeSelectOption/optimizeSelectOption', //  tabl扩展
 });
