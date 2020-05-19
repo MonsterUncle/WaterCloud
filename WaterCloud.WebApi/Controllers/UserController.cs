eb8d2046c26b110dec27241ef6f747b9d0873922
钱玮鸿 <7353672+qian_wei_hong@users.noreply.gitee.com>
2020-05-19 16:16:39 +0800

    增加WebApi示例 登录获取令牌

---
+++ WaterCloud.WebApi/Controllers/UserController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Senparc.CO2NET.Extensions;
+using WaterCloud.Code;
+using WaterCloud.Domain.SystemManage;
+using WaterCloud.Domain.SystemSecurity;
+using WaterCloud.Service;
+using WaterCloud.Service.SystemManage;
+using WaterCloud.Service.SystemSecurity;
+
+namespace WaterCloud.WebApi.Controllers
+{
+    /// <summary>
+    /// 用户接口
+    /// </summary>
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserService _userService = new UserService();
+        private LogService _logService = new LogService();
+        private FilterIPService _filterIPService = new FilterIPService();
+
+        #region 获取数据       
+        #endregion
+
+        #region 提交数据
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Login([FromQuery] string userName, [FromQuery] string password, [FromQuery] string token)
+        {
+            var apitoken = Utils.GuId();
+            if (!string.IsNullOrEmpty(token))
+            {
+                apitoken = token;
+            }
+
+            LogEntity logEntity = new LogEntity();
+            logEntity.F_ModuleName = "用户Api";
+            logEntity.F_Type = DbLogType.Login.ToString();
+            try
+            {
+                if (!await CheckIP())
+                {
+                    throw new Exception("IP受限");
+                }
+                UserEntity userEntity = await _userService.CheckLogin(userName, Md5.md5(password, 32).ToLower(), token);
+                OperatorModel operatorModel = new OperatorModel();
+                operatorModel.UserId = userEntity.F_Id;
+                operatorModel.UserCode = userEntity.F_Account;
+                operatorModel.UserName = userEntity.F_RealName;
+                operatorModel.CompanyId = userEntity.F_OrganizeId;
+                operatorModel.DepartmentId = userEntity.F_DepartmentId;
+                operatorModel.RoleId = userEntity.F_RoleId;
+                operatorModel.LoginIPAddress = Request.HttpContext.Connection.LocalIpAddress.MapToIPv4().ToString(); 
+                operatorModel.LoginIPAddressName = "本地局域网";//Net.GetLocation(operatorModel.LoginIPAddress);
+                operatorModel.LoginTime = DateTime.Now;
+                operatorModel.DdUserId = userEntity.F_DingTalkUserId;
+                operatorModel.WxOpenId = userEntity.F_WxOpenId;
+                operatorModel.IsAdmin = userEntity.F_IsAdmin.Value;
+                operatorModel.IsBoss = userEntity.F_IsBoss.Value;
+                operatorModel.IsLeaderInDepts = userEntity.F_IsLeaderInDepts.Value;
+                operatorModel.IsSenior = userEntity.F_IsSenior.Value;
+                if (userEntity.F_Account == "admin")
+                {
+                    operatorModel.IsSystem = true;
+                }
+                else
+                {
+                    operatorModel.IsSystem = false;
+                }
+                await OperatorProvider.Provider.AddLoginUser(operatorModel, apitoken, "api_",false);
+                logEntity.F_Account = userEntity.F_Account;
+                logEntity.F_NickName = userEntity.F_RealName;
+                logEntity.F_Result = true;
+                logEntity.F_Description = "登录成功";
+                await _logService.WriteDbLog(logEntity);
+                return Content(new AjaxResult<string> { state = ResultType.success.ToString(), message = "登录成功。",data= apitoken }.ToJson());
+            }
+            catch (Exception ex)
+            {
+                logEntity.F_Account = userName;
+                logEntity.F_NickName = userName;
+                logEntity.F_Result = false;
+                logEntity.F_Description = "登录失败，" + ex.Message;
+                await _logService.WriteDbLog(logEntity);
+                return Content(new AjaxResult<string> { state = ResultType.error.ToString(), message = ex.Message,data= apitoken }.ToJson());
+            }
+        }
+        private async Task<bool> CheckIP()
+        {
+            string ip = Request.HttpContext.Connection.LocalIpAddress.MapToIPv4().ToString();
+            return await _filterIPService.CheckIP(ip);
+        }
+
+        /// <summary>
+        /// 用户退出登录
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> LoginOff([FromQuery] string token)
+        {
+            await _logService.WriteDbLog(new LogEntity
+            {
+                F_ModuleName = "用户Api",
+                F_Type = DbLogType.Exit.ToString(),
+                F_Account = OperatorProvider.Provider.GetCurrent(token).UserCode,
+                F_NickName = OperatorProvider.Provider.GetCurrent(token).UserName,
+                F_Result = true,
+                F_Description = "安全退出系统",
+            });
+            await OperatorProvider.Provider.EmptyCurrent("api_",token);
+            return Content(new AjaxResult { state = ResultType.success.ToString() }.ToJson());
+        }
+        #endregion
+    }
+}
---
+++ WaterCloud.WebApi/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Reflection;
+using CSRedis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using WaterCloud.Code;
+using WaterCloud.Code.Model;
+
+namespace WaterCloud.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            GlobalContext.LogWhenStart(env);
+            GlobalContext.HostingEnvironment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo { Title = "WaterCloud Api", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                config.IncludeXmlComments(xmlPath, true); //添加控制器层注释（true表示显示控制器注释）                
+            });
+            //redis 注入服务
+            string redisConnectiong = Configuration.GetSection("SystemConfig:RedisConnectionString").Value;
+            // 多客户端
+            var redisDB = new CSRedisClient(redisConnectiong + ",defaultDatabase=" + 0);
+            RedisHelper.Initialization(redisDB);
+            //注册服务
+            services.AddSingleton(redisDB);
+            services.AddOptions();
+            services.AddCors();
+            services.AddControllers(options =>
+            {
+                options.ModelMetadataDetailsProviders.Add(new ModelBindingMetadataProvider());
+            }).AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
+            services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(GlobalContext.HostingEnvironment.ContentRootPath + Path.DirectorySeparatorChar + "DataProtection"));
+            GlobalContext.SystemConfig = Configuration.GetSection("SystemConfig").Get<SystemConfig>();
+            GlobalContext.Services = services;
+            GlobalContext.Configuration = Configuration;
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = GlobalContext.SetCacheControl
+            });
+            app.UseCors(builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins(GlobalContext.SystemConfig.AllowCorsSite.Split(',')).AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+            });
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "api-doc/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "api-doc";
+                c.SwaggerEndpoint("v1/swagger.json", "WaterCloud Api v1");
+            });
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=ApiHome}/{action=Index}/{id?}");
+            });
+            GlobalContext.ServiceProvider = app.ApplicationServices;
+        }
+    }
+}
