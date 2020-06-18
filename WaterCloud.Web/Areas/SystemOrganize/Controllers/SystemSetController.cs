1930cd16a4aa8751dfbbd12b2f25c1cd969b2b4e
钱玮鸿 <295228902@qq.com>
2020-06-18 10:18:56 +0800

    图片上传问题

--- WaterCloud.Web/Areas/SystemOrganize/Controllers/SystemSetController.cs
+++ WaterCloud.Web/Areas/SystemOrganize/Controllers/SystemSetController.cs
@@ -151,7 +151,11 @@ namespace WaterCloud.Web.Areas.SystemOrganize.Controllers
                 {
                     throw new Exception("请上传图片");
                 }
-                string filePath = GlobalContext.HostingEnvironment.WebRootPath + $@"\icon\"+stemp+ $@"\";
+                string filePath = GlobalContext.HostingEnvironment.WebRootPath + $@"\icon\";
+                if (!string.IsNullOrEmpty(stemp))
+                {
+                    filePath = GlobalContext.HostingEnvironment.WebRootPath + $@"\icon\" + stemp + $@"\";
+                }
                 if (!Directory.Exists(filePath))
                 {
                     Directory.CreateDirectory(filePath);
