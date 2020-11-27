590c5eb52430cf37daad4bcc3b99c133aee6e563
MonstorUncle <295228902@qq.com>
2020-11-27 15:50:36 +0800

    update WaterCloud.Service/TimeService/TimedService.cs. 异步以同步形式调用

--- WaterCloud.Service/TimeService/TimedService.cs
+++ WaterCloud.Service/TimeService/TimedService.cs
@@ -1,4 +1,4 @@
-﻿using Chloe;
+using Chloe;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -59,7 +59,7 @@ namespace WaterCloud.Service.TimeService
             entity.F_CPU = computer.CPURate;
             entity.F_IIS = "0";
             entity.F_WebSite = _hostingEnvironment.ContentRootPath;
-            new ServerStateService(_context).SubmitForm(entity);
+            new ServerStateService(_context).SubmitForm(entity).GetAwaiter().GetResult();
         }
 
         public override void Dispose()
