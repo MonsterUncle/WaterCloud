51cb712d6a9ce945e30bdc075078520f15b87449
MonstorUncle <295228902@qq.com>
2020-11-30 14:13:31 +0800

    1.6.0 修复更新bug

--- WaterCloud.CodeGenerator/Template/SingleTableTemplate.cs
+++ WaterCloud.CodeGenerator/Template/SingleTableTemplate.cs
@@ -289,7 +289,7 @@ namespace WaterCloud.CodeGenerator
                     {
                         if (column != deleteMarkField && column != createTimeField)
                         {
-                            sb.AppendLine($"                    entity.{column} = //添写初始化参数;");
+                            sb.AppendLine($"                entity.{column} = ;//添写初始化参数;");
                         }
                     }
                 }
