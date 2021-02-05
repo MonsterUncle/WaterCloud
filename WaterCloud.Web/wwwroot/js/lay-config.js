441b1f88106c000da6d182efcf1080f1b81849b5
MonstorUncle <295228902@qq.com>
2021-02-05 14:50:06 +0800

    2.3.0 删除多余插件，合并echarts，删掉layui带的echarts主题

--- WaterCloud.Web/wwwroot/js/lay-config.js
+++ WaterCloud.Web/wwwroot/js/lay-config.js
@@ -25,7 +25,6 @@ layui.config({
     tableSelect: 'tableSelect/tableSelect', // table选择扩展
     iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
     echarts: 'echarts/echarts', // echarts图表扩展
-    echartsTheme: 'echarts/echartsTheme', // echarts图表主题扩展
     wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
     layarea: 'layarea/layarea', //  省市县区三级联动下拉选择器
     common: 'waterCloud/common', //  工具类
