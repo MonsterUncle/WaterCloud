e567336e6e04c9d8caf7aa15f560eaa5ae0ec4e8
MonstorUncle <295228902@qq.com>
2021-02-25 14:03:22 +0800

    2.4.0 版本退回异常

--- WaterCloud.Web/wwwroot/js/lay-module/treetable-lay/treeTable.js
+++ WaterCloud.Web/wwwroot/js/lay-module/treetable-lay/treeTable.js
@@ -521,17 +521,19 @@ layui.define(['laytpl', 'form', 'util'], function (exports) {
             d.LAY_CHECKED = checked;  // 同时更新数据
             d.LAY_INDETERMINATE = false;
             // 联动操作
-            if (d[options.tree.childName] && d[options.tree.childName].length > 0) {
-                that.checkSubCB($tr, checked);  // 联动子级
-            }
-            var indent = parseInt($tr.data('indent'));
-            $tr.prevAll('tr').each(function () {
-                var tInd = parseInt($(this).data('indent'));
-                if (tInd < indent) {
-                    that.checkParentCB($(this));  // 联动父级
-                    indent = tInd;
+            if (options.checkOther) {
+                if (d[options.tree.childName] && d[options.tree.childName].length > 0) {
+                    that.checkSubCB($tr, checked);  // 联动子级
                 }
-            });
+                var indent = parseInt($tr.data('indent'));
+                $tr.prevAll('tr').each(function () {
+                    var tInd = parseInt($(this).data('indent'));
+                    if (tInd < indent) {
+                        that.checkParentCB($(this));  // 联动父级
+                        indent = tInd;
+                    }
+                });
+            }
             that.checkChooseAllCB();  // 联动全选框
             // 回调事件
             layui.event.call(this, MOD_NAME, 'checkbox(' + components.filter + ')',
