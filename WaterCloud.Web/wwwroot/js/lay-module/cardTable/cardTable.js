8626dc610d6aa9c659d2b5343e18784e41dd6dfb
MonstorUncle <295228902@qq.com>
2021-03-22 20:49:54 +0800

    2.6.0 删除cardtable中的 缓存验证

--- WaterCloud.Web/wwwroot/js/lay-module/cardTable/cardTable.js
+++ WaterCloud.Web/wwwroot/js/lay-module/cardTable/cardTable.js
@@ -66,13 +66,6 @@ layui.define(['table', 'laypage','jquery', 'element'], function(exports) {
 		// 根 据 请 求 方 式 获 取 数 据
 		html = "";
 		if (url != null) {
-			if (url.indexOf("?") >= 0) {
-				url = url + '&v=' + new Date().Format("yyyy-MM-dd hh:mm:ss");
-			}
-			else {
-				url = url + '?v=' + new Date().Format("yyyy-MM-dd hh:mm:ss");
-
-			}
 			if (!!option.page) {
 				url = url + '&' + option.request.limitName + '=' + option.limit;
 				url = url + '&' + option.request.pageName + '=' + option.currentPage;
@@ -213,18 +206,3 @@ layui.define(['table', 'laypage','jquery', 'element'], function(exports) {
 	}
 	exports(MOD_NAME, tt);
 })
-Date.prototype.Format = function (fmt) {
-	var o = {
-		"M+": this.getMonth() + 1, //月份 
-		"d+": this.getDate(), //日 
-		"h+": this.getHours(), //小时 
-		"m+": this.getMinutes(), //分 
-		"s+": this.getSeconds(), //秒 
-		"q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-		"S": this.getMilliseconds() //毫秒 
-	};
-	if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-	for (var k in o)
-		if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-	return fmt;
-}
