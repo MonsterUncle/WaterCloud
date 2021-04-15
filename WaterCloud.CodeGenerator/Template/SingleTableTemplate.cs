0782e0a359759e46dcd4577b12ae643abf21dd3d
MonstorUncle <295228902@qq.com>
2021-04-15 21:04:44 +0800

    2.7.0 修改代码生成

--- WaterCloud.CodeGenerator/Template/SingleTableTemplate.cs
+++ WaterCloud.CodeGenerator/Template/SingleTableTemplate.cs
@@ -233,7 +233,7 @@ namespace WaterCloud.CodeGenerator
                     }
                 }
                 sb.AppendLine("            }");
-                sb.AppendLine("            return data."+(string.IsNullOrEmpty(baseConfigModel.PageIndex.DeleteColum)?"": $"Where(t => t.{baseConfigModel.PageIndex.DeleteColum} == false).") + (baseConfigModel.PageIndex.IsAsc == true ? "OrderBy" : "OrderByDesc") + $"(t => t.{baseConfigModel.PageIndex.SortColumn}).ToList();");
+                sb.AppendLine("            return data." + (string.IsNullOrEmpty(baseConfigModel.PageIndex.DeleteColum) ? "" : $"Where(t => t.{baseConfigModel.PageIndex.DeleteColum} == false).") + (baseConfigModel.PageIndex.IsAsc == true ? "OrderBy" : "OrderByDesc") + $"(t => t.{baseConfigModel.PageIndex.SortColumn}).ToList();");
             }
             else
             {
@@ -342,7 +342,7 @@ namespace WaterCloud.CodeGenerator
             sb.AppendLine("        }");
             sb.AppendLine();
             sb.AppendLine("        #region 提交数据");
-            sb.AppendLine("        public async Task SubmitForm(" + baseConfigModel.FileConfig.EntityName + $" entity, {idType} keyValue)");
+            sb.AppendLine("        public async Task SubmitForm(" + baseConfigModel.FileConfig.EntityName + $" entity, {idType} keyValue" + idType == "int" || idType == "long" ? "=0)" : ")");
             sb.AppendLine("        {");
             if (idType == "int" || idType == "long")
             {
@@ -350,17 +350,17 @@ namespace WaterCloud.CodeGenerator
             }
             else
             {
-                sb.AppendLine("            if(!string.IsNullOrEmpty(keyValue))");
+                sb.AppendLine("            if(string.IsNullOrEmpty(keyValue))");
             }
             sb.AppendLine("            {");
             sb.AppendLine("                    //初始值添加");
-			if (!string.IsNullOrEmpty(baseConfigModel.PageIndex.DeleteColum))
-			{
+            if (!string.IsNullOrEmpty(baseConfigModel.PageIndex.DeleteColum))
+            {
                 sb.AppendLine($"                entity.{baseConfigModel.PageIndex.DeleteColum} = false;");
             }
             foreach (var item in baseConfigModel.PageIndex.ColumnList)
             {
-                if (item.field != idColumn&& item.field != baseConfigModel.PageIndex.DeleteColum && item.field != baseConfigModel.PageIndex.CreateColum && !string.IsNullOrEmpty(item.value))
+                if (item.field != idColumn && item.field != baseConfigModel.PageIndex.DeleteColum && item.field != baseConfigModel.PageIndex.CreateColum && !string.IsNullOrEmpty(item.value))
                 {
                     sb.AppendLine($"                entity.{item.field} = {item.value};");
                 }
@@ -1015,7 +1015,7 @@ namespace WaterCloud.CodeGenerator
                             sb.AppendLine("                            <input type=\"text\" id=\"" + item.Key + "\" name=\"" + item.Key + "\" autocomplete=\"off\" lay-verify=\"required\" placeholder=\"请输入\" class=\"layui-input\">");
                             sb.AppendLine("                        </div>");
                             sb.AppendLine("                    </div>");
-                            if (i % 2 == 0 )
+                            if (i % 2 == 0)
                             {
                                 sb.AppendLine("                </div>");
                             }
