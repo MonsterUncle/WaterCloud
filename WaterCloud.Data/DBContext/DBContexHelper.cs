61bf58d522a68576f31df512c780954cb7fed459
MonstorUncle <295228902@qq.com>
2021-05-11 15:42:54 +0800

    2.9.0 修改dbhelper文件

--- WaterCloud.Data/DBContext/DBContexHelper.cs
+++ WaterCloud.Data/DBContext/DBContexHelper.cs
@@ -17,30 +17,31 @@ namespace WaterCloud.DataBase
         {
             ConnectStr = string.IsNullOrEmpty(ConnectStr) ? dbConnectionString : ConnectStr;
             providerName = string.IsNullOrEmpty(providerName) ? dbType : providerName;
-            IDbContext context;
-            switch (providerName)
+            if (providerName == Define.DBTYPE_SQLSERVER)
             {
-                case Define.DBTYPE_SQLSERVER:
-                    SqlConnection.ClearAllPools();
-                    context = new MsSqlContext(new MSSqlConnectionFactory(ConnectStr));
-                    context.Session.CommandTimeout = int.Parse(DBCommandTimeout);
-                    break;
-                case Define.DBTYPE_MYSQL:
-                    context = new MySqlContext(new MySqlConnectionFactory(ConnectStr));
-                    context.Session.CommandTimeout = int.Parse(DBCommandTimeout);
-                    break;
-                case Define.DBTYPE_ORACLE:
-                    var con = new OracleContext(new OracleConnectionFactory(ConnectStr));
-                    con.Session.CommandTimeout = int.Parse(DBCommandTimeout);
-                    con.ConvertToUppercase = true;
-                    context = con;
-                    break;
-                default:
-                    context = new MsSqlContext(ConnectStr);
-                    context.Session.CommandTimeout = int.Parse(DBCommandTimeout);
-                    break;
+                var dbContext = new MsSqlContext(new MSSqlConnectionFactory(ConnectStr));
+                //2012以上版本切换使用 OFFSET FETCH 分页方式
+                //dbContext.PagingMode = PagingMode.OFFSET_FETCH;
+                dbContext.Session.CommandTimeout = int.Parse(DBCommandTimeout);
+                return dbContext;
             }
-            return context;
+            else if (providerName == Define.DBTYPE_MYSQL)
+            {
+                var dbContext = new MySqlContext(new MySqlConnectionFactory(ConnectStr));
+                dbContext.Session.CommandTimeout = int.Parse(DBCommandTimeout);
+                return dbContext;
+            }
+            else if (providerName == Define.DBTYPE_ORACLE)
+            {
+                var dbContext = new OracleContext(new OracleConnectionFactory(ConnectStr));
+                dbContext.Session.CommandTimeout = int.Parse(DBCommandTimeout);
+                dbContext.ConvertToUppercase = false;
+                return dbContext;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
