d51c870658859197274c8a10221f4c93469d4e21
MonstorUncle <295228902@qq.com>
2021-05-11 21:36:36 +0800

    2.9.0 配置数据库类型简化

--- WaterCloud.Code/Model/Define.cs
+++ WaterCloud.Code/Model/Define.cs
@@ -6,9 +6,9 @@ namespace WaterCloud.Code
 {
     public static class Define
     {
-        public const string DBTYPE_SQLSERVER = "System.Data.SqlClient";    //sql server
-        public const string DBTYPE_MYSQL = "MySql.Data.MySqlClient";    //sql server
-        public const string DBTYPE_ORACLE = "Oracle.ManagedDataAccess.Client";    //oracle
+        public const string DBTYPE_SQLSERVER = "MSSql";    //sql server
+        public const string DBTYPE_MYSQL = "MySql";    //sql server
+        public const string DBTYPE_ORACLE = "Oracle";    //oracle
 
         public const string PROVIDER_COOKIE = "Cookie";
         public const string PROVIDER_SESSION = "Session";
