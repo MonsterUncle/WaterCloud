5ec80bd2456c261dc179af988c6fc4e29ae8b079
MonstorUncle <295228902@qq.com>
2021-06-09 16:24:03 +0800

    2.11.0 增加SqlSugar二级缓存类

---
+++ WaterCloud.Data/SqlSugarCache/SqlSugarRedisCache.cs
@@ -0,0 +1,56 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using WaterCloud.Code;
+
+namespace WaterCloud.DataBase
+{
+    public class SqlSugarRedisCache : ICacheService
+    {
+        public void Add<TV>(string key, TV value)
+        {
+            BaseHelper.Set(key, value);
+        }
+
+        public void Add<TV>(string key, TV value, int cacheDurationInSeconds)
+        {
+            BaseHelper.Set(key, value, cacheDurationInSeconds);
+        }
+
+        public bool ContainsKey<TV>(string key)
+        {
+            return BaseHelper.Exists(key);
+        }
+
+        public TV Get<TV>(string key)
+        {
+            return BaseHelper.Get<TV>(key);
+        }
+
+        public IEnumerable<string> GetAllKey<TV>()
+        {
+
+            return BaseHelper.Keys("SqlSugarDataCache.*");
+        }
+
+        public TV GetOrCreate<TV>(string cacheKey, Func<TV> create, int cacheDurationInSeconds = int.MaxValue)
+        {
+            if (this.ContainsKey<TV>(cacheKey))
+            {
+                return this.Get<TV>(cacheKey);
+            }
+            else
+            {
+                var result = create();
+                this.Add(cacheKey, result, cacheDurationInSeconds);
+                return result;
+            }
+        }
+
+        public void Remove<TV>(string key)
+        {
+            BaseHelper.Del(key);
+        }
+    }
+
+}
