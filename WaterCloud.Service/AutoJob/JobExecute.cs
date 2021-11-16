8b3482460b8dd1cad7d978c83cb320f6480662e6
冬天 <szwinter@qq.com>
2021-11-16 03:22:31 +0000

    update WaterCloud.Service/AutoJob/JobExecute.cs.

--- WaterCloud.Service/AutoJob/JobExecute.cs
+++ WaterCloud.Service/AutoJob/JobExecute.cs
@@ -62,91 +62,9 @@ namespace WaterCloud.Service.AutoJob
                                         await _scheduler.RescheduleJob(trigger.Key, trigger);
                                         return;
                                     }
-                                    #region 执行任务
-                                    OpenJobLogEntity log = new OpenJobLogEntity();
-                                    log.F_Id = Utils.GuId();
-                                    log.F_JobId = jobId;
-                                    log.F_JobName = dbJobEntity.F_JobName;
-                                    log.F_CreatorTime = now;
-                                    unitwork.CurrentBeginTrans();
-                                    AlwaysResult result = new AlwaysResult();
-                                    if (dbJobEntity.F_JobType == 0)
-                                    {
-                                        //反射执行就行
-                                        var path = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
-                                        //反射取指定前后缀的dll
-                                        var referencedAssemblies = Directory.GetFiles(path, "WaterCloud.*.dll").Select(Assembly.LoadFrom).ToArray();
-                                        var types = referencedAssemblies
-                                            .SelectMany(a => a.GetTypes().Where(t => t.GetInterfaces()
-                                            .Contains(typeof(IJobTask)))).ToArray();
-                                        string filename = dbJobEntity.F_FileName;
-                                        var implementType = types.Where(x => x.IsClass && x.FullName == filename).FirstOrDefault();
-                                        unitwork.GetDbClient().ChangeDatabase(dbJobEntity.F_DbNumber);
-                                        var obj = System.Activator.CreateInstance(implementType, unitwork);       // 创建实例(带参数)
-                                        MethodInfo method = implementType.GetMethod("Start", new Type[] { });      // 获取方法信息
-                                        object[] parameters = null;
-                                        result = ((Task<AlwaysResult>)method.Invoke(obj, parameters)).GetAwaiter().GetResult();     // 调用方法，参数为空
-                                        if (result.state.ToString() == ResultType.success.ToString())
-                                        {
-                                            log.F_EnabledMark = true;
-                                            log.F_Description = "执行成功，" + result.message.ToString();
-                                        }
-                                        else
-                                        {
-                                            log.F_EnabledMark = false;
-                                            log.F_Description = "执行失败，" + result.message.ToString();
-                                        }
-                                    }
-                                    else
-                                    {
-                                        HttpMethod method = HttpMethod.Get;
-                                        switch (dbJobEntity.F_JobType)
-                                        {
-                                            case 1:
-                                                method = HttpMethod.Get;
-                                                break;
-                                            case 2:
-                                                method = HttpMethod.Post;
-                                                break;
-                                            case 3:
-                                                method = HttpMethod.Put;
-                                                break;
-                                            case 4:
-                                                method = HttpMethod.Delete;
-                                                break;
-                                        }
-                                        var dic = dbJobEntity.F_RequestHeaders.ToObject<Dictionary<string, string>>();
-                                        //请求头添加租户号
-                                        dic.Add("dbNumber",dbJobEntity.F_DbNumber);
-                                        try
-                                        {
-                                            var temp = await new HttpWebClient(_httpClient).ExecuteAsync(dbJobEntity.F_RequestUrl, method, dbJobEntity.F_RequestString, dic);
-                                            log.F_EnabledMark = true;
-                                            log.F_Description = "执行成功。";
-                                        }
-                                        catch (Exception ex)
-                                        {
-                                            log.F_EnabledMark = false;
-                                            log.F_Description = "执行失败，" + ex.Message.ToString();
-                                        }
-                                    }
-                                    unitwork.GetDbClient().ChangeDatabase("0");
-                                    #endregion
-                                    await unitwork.GetDbClient().Updateable<OpenJobEntity>(t => new OpenJobEntity
-                                    {
-                                        F_LastRunTime = now
-                                    }).Where(t => t.F_Id == dbJobEntity.F_Id).ExecuteCommandAsync();
-                                    string HandleLogProvider = GlobalContext.SystemConfig.HandleLogProvider;
-                                    if (HandleLogProvider != Define.CACHEPROVIDER_REDIS)
-                                    {
-                                        unitwork.GetDbClient().Insertable(log).ExecuteCommand();
-                                    }
-                                    else
-                                    {
-                                        await HandleLogHelper.HSetAsync(log.F_JobId, log.F_Id, log);
-                                    }
-                                    unitwork.CurrentCommit();
                                 }
+
+                                await autoJobService.JobExecute(dbJobEntity, unitwork);
                             }
                         }
                     }
