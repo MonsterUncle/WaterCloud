1e3acdc8af2550c52c89d0a99eef17dd5a84142d
12888491 <295228902@qq.com>
2022-02-10 15:28:46 +0800

    2.19.3 修复首页粒子特效

--- WaterCloud.Web/wwwroot/js/login.js
+++ WaterCloud.Web/wwwroot/js/login.js
@@ -1,4 +1,23 @@
-﻿function draw(show_num) {
+﻿//粒子特效
+var cols = ['#f5d76e', '#f7ca18', '#f4d03f', '#ececec', '#ecf0f1', '#a2ded0'];
+var stars = 250;
+for (var i = 0; i <= stars; i++) {
+    var size = Math.random() * 3;
+    var color = cols[parseInt(Math.random() * 4)];
+
+    $('#starsBox').prepend('<span style=" width: ' + size + 'px; height: ' + size + 'px; top: ' + Math.random() * 100 + '%; left: ' + Math.random() * 100 + '%; background: ' + color + '; box-shadow: 0 0 ' + Math.random() * 10 + 'px' + color + ';"></span>');
+};
+setTimeout(function () {
+    $('#starsBox span').each(function () {
+        $(this).css('top', Math.random() * 100 + '%').css('left', Math.random() * 100 + '%');
+    });
+}, 1);
+setInterval(function () {
+    $('#starsBox span').each(function () {
+        $(this).css('top', Math.random() * 100 + '%').css('left', Math.random() * 100 + '%');
+    });
+}, 100000);
+function draw(show_num) {
     var canvas_width = $('#canvas').width();
     var canvas_height = $('#canvas').height();
     var canvas = document.getElementById("canvas");//获取到canvas的对象，演员
