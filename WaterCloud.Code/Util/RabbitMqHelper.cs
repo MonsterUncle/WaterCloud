1fd7cf007490b82bab9a42e74aece59304ac08fd
MonstorUncle <295228902@qq.com>
2022-04-20 20:14:50 +0800

    注释

--- WaterCloud.Code/Util/RabbitMqHelper.cs
+++ WaterCloud.Code/Util/RabbitMqHelper.cs
@@ -1831,7 +1831,7 @@ namespace WaterCloud.Code
         public string Exchange { get; set; }
 
         /// <summary>
-        /// 交换机类型(广播fanout，订阅direct)
+        /// 交换机类型(广播fanout，队列direct)
         /// </summary>
         public string ExchangeType { get; set; }
 
