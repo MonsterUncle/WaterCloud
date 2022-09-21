f728b8a661238cada32b721802a121bdcfa581e7
12888491 <295228902@qq.com>
2022-09-21 15:48:25 +0800

    移动api文件

---
+++ WaterCloud.Web/Controllers/Api/TestController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WaterCloud.Web
+{
+    /// <summary>
+    /// 测试文件
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiExplorerSettings(GroupName = "V2")]
+    [ApiController]
+    [LoginFilter]
+    public class TestController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody, Required(ErrorMessage = "值不能为空")] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody, Required(ErrorMessage = "值不能为空")] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
